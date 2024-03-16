e8aa31deacc11ac594ad307ea801cd2e648f4196
Yathart Kaushal <yathartkaushal@proton.me>
2024-03-16 18:57:26 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import CustomNavbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <>
-      <div>hello world</div>
+      <CustomNavbar />
+      <h1 className="font-bold text-2xl w-full bg-gray-950">Projects</h1>
+      <br />
+
+      <div className="flex gap-4 p-4">
+        <div className="flex container w-52 h-28 bg-gray-950 p-4 text-center rounded-md gap-4">
+          <p className="m-auto font-medium">Create New Project</p>
+        </div>
+        <div className="flex-col container w-52 h-28 bg-gray-950 p-2 rounded-md">
+          <p className="font-bold overflow-hidden">Title</p>
+          <p className="font-medium line-clamp-3">Description</p>
+          <p className="mt-6 align-bottom text-gray-700">
+            Created <span className="font-medium">3 days ago</span>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
